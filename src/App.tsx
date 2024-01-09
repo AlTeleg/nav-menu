641b52734cabff604505d2628a86d1d334aa87b3
Александр <alteleg@yandex.ru>
2024-01-09 19:23:29 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import HomePage from './components/HomePage';
+import DriftPage from './components/DriftPage';
+import TimeAttackPage from './components/TimeAttackPage';
+import ForzaPage from './components/ForzaPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/drift" element={<DriftPage/>} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage/>} />
+          </Routes>
+
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
